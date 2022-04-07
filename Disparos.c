56f08bb99d39e8741b353cb6c231cc5919902efa
Naomig06 <74263800+Naomig06@users.noreply.github.com>
2022-04-06 22:55:19 -0400

    Update Disparos.c

--- Disparos.c
+++ Disparos.c
@@ -32,6 +32,7 @@ int  A[TOPEY][TOPEX];
 int  x;
 int  y;
 int  p = 2;
+int minutos, segundos, stop;
 
 void fnarraygeneral    ();
 void FnSetColor        (int, int);
@@ -251,7 +252,28 @@ void fnimprimemarco(int fi, int ci, int ff, int cf){
 	return;
 }
 
+void fntimer(int x, int y) {
 
+fngotoxy(198,5);
+printf("TIMER: \n");
+
+stop = 0;
+    for (minutos=0;((minutos<60)&&(stop==0)); minutos++){
+
+      for(segundos=0;((segundos<60)&&(stop==0));segundos++) {
+        fngotoxy(198,5);
+        printf("%d : %d", minutos, segundos);
+        Sleep(1000);
+      }
+
+    }
+    /*
+fngotoxy(45,15);
+printf("TOTAL TIME:\n");
+printf("%d : %d", minutos, segundos);
+*/
+return
+}
 
 void fngotoxy(int x, int y){
       HANDLE hcon;
